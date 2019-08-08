e76312784683c784432165f5f589d854d98ea555
Businka2611 <moszechok@gmail.com>
2019-08-08 14:49:29 +0300

    Add close error method

--- public/js/buttonsHandler.js
+++ public/js/buttonsHandler.js
@@ -1,3 +1,5 @@
+import {ClassHelper} from './classHelper.js';
+
 function ButtonsHandler() {}
 
 ButtonsHandler.prototype.init = function(arr, handler) {
@@ -19,6 +21,11 @@ function onComplete() {
         .catch(err => console.log(err));
 }
 
+function closeError() {
+    ClassHelper.addClass('hidden', [this.parentNode]);
+}
+
 ButtonsHandler.onComplete = onComplete;
+ButtonsHandler.closeError = closeError;
 
 export { ButtonsHandler };
--- public/js/common.js
+++ public/js/common.js
@@ -4,10 +4,14 @@ const HTMLelements = {
   tbody: document.querySelectorAll(".aim__tbody"),
   save: document.querySelectorAll(".save.icon"),
   edit: document.querySelectorAll(".edit.icon"),
-  delete: document.querySelectorAll(".delete.icon")
+  delete: document.querySelectorAll(".delete.icon"),
+  close: document.querySelectorAll(".icon_close")
 };
 
-let onButtonClick = new ButtonsHandler();
-onButtonClick.init(HTMLelements.save, ButtonsHandler.onComplete);
+let onCompleteClick = new ButtonsHandler();
+onCompleteClick.init(HTMLelements.save, ButtonsHandler.onComplete);
+
+let onCloseClick = new ButtonsHandler();
+onCloseClick.init(HTMLelements.close, ButtonsHandler.closeError);
 
 export { HTMLelements };
